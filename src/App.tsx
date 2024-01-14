ef4a1d6a45f80db42934be34024cf3aad7bbed55
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-01-14 02:39:11 +0200

    adding: fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="text-3xl font-bold underline">hi</div>;
+  return <div className="text-3xl font-bold underline font-titan">hi</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      mochiy: ['"Mochiy Pop One"', 'sans-serif'],
+      Paytone: ['"Paytone One"', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+      titan: ['"Titan One"', 'sans-serif'],
+    },
   },
   plugins: [],
 };
