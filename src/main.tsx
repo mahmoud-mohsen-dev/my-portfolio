885894b6b76190c9b0f5bd13bd270edac3df0a4e
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-01-23 07:14:04 +0200

    adding vercel web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'tw-elements-react/dist/css/tw-elements-react.min.css';
 import './index.css';
-
+import { inject } from '@vercel/analytics';
+inject();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
