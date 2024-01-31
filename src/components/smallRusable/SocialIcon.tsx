ef85f8bdfcf088abab09baaf4eabb5fb86caf992
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-01-31 09:07:53 +0200

    changing mail icon

--- src/components/smallRusable/SocialIcon.tsx
+++ src/components/smallRusable/SocialIcon.tsx
@@ -4,7 +4,7 @@ function SocialIcon({ className, href, src, alt }: { className?: string; href: s
       rel="noreferrer"
       target="_blank"
       href={href}
-      className={`transition-transform duration-300 ease-transform hover:-translate-y-2 ${className}`}
+      className={` flex justify-center items-center transition-transform duration-300 ease-transform hover:-translate-y-2 ${className}`}
     >
       <img src={src} alt={alt} />
     </a>
